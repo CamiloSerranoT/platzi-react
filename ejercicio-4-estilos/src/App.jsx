6f9b5e53dc0cc0aa1f90554639ab967e88022f9c
CamiloSerranoT <camilo.a.serrano.t@gmail.com>
2025-11-11 11:44:17 -0500

    Uso de Tailwind

--- ejercicio-4-estilos/src/App.jsx
+++ ejercicio-4-estilos/src/App.jsx
@@ -1,11 +1,17 @@
 import Card from "./components/Card/Card"
 import Button from "./components/Button/Button"
+import Text from "./components/Text/Text"
+
+/*
+  Sección en return
+  <Button><p>Gris</p></Button>
+  <Button blue><p>Azul</p></Button>
+*/
 
 function App() {
   return (
     <>
-      <Button><p>Gris</p></Button>
-      <Button blue><p>Azul</p></Button>
+      <Text />
     </>
   )
 }
---
+++ ejercicio-4-estilos/src/components/Text/Text.jsx
@@ -0,0 +1,11 @@
+const TailwindText = () => {
+    return (
+        <div class="shadow-xl ... bg-gray-400 ...">
+            <h1 class="text-3xl font-bold underline text-white">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
+export default TailwindText;
