5bb0c9fee9f425f4c47561143d411f26bb1caa5f
CamiloSerranoTProjekt <camilo@projektagency.com.au>
2025-11-11 22:20:29 -0500

    Errores de carga, fetch y dependencias

---
+++ ejercicio-5-estaticos/src/App.jsx
@@ -0,0 +1,18 @@
+import StaticComponent from './components/StaticComponent/StaticComponent'
+import UserList from './components/UserList/UserList'
+import SearchPosts from './components/SearchPosts/SearchPosts'
+import './App.css'
+
+//<StaticComponent />
+//<UserList />
+
+function App() {
+  
+  return (
+    <>
+      <SearchPosts />
+    </>
+  )
+}
+
+export default App
---
+++ ejercicio-5-estaticos/src/components/SearchPosts/SearchPosts.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const SearchPosts = () => {
+    const [query, setQuery] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?title_like=${query}`)
+        .then(response => response.json())
+        .then(data => setPosts(data));
+    }, [query]);
+
+    return (
+    <div>
+        <h2>Lista de usuarios</h2>
+        <input 
+        placeholder="Buscar por título" 
+        value={query} 
+        onChange={(event) => setQuery(event.target.value)} 
+        />
+        <ul>
+        {posts.map(post => (
+            <li key={post.id}>{post.title}</li>
+        ))}
+        </ul>
+    </div>
+    );
+}
+
+export default SearchPosts;
